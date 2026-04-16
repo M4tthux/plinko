9e37a7b0d865bd57c4b6fb0e6e051ac992cf47c8
M4tthux <M4tthux@users.noreply.github.com>
2026-04-16 22:17:52 +0000

    deploy: eb43961d20b368ff9ca02ee7b4726373442de14c

--- main.dart.js
+++ main.dart.js
@@ -24850,7 +24850,7 @@ $S:2}
 A.aks.prototype={
 $1(a){var s=A.cM().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/59aa584fdf100e6c78c785d8a5b565d1de4b48ab/":s)+a},
 $S:113}
 A.FO.prototype={
 TD(){var s=this.a7T(),r=$.aS.bk().ImageFilter.MakeColorFilter(s,null)
